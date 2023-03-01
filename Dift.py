34fdba56b11ebe1c90fbfcc1bb9b619ea3b3270f
NisheshTyagi <115863752+NisheshTyagi@users.noreply.github.com>
2023-03-01 15:28:35 +0530

    Added comments in Dift.py
    Added comments to Python library code for improved documentation and readability.
    No changes were made to the functionality of the library.

--- Dift.py
+++ Dift.py
@@ -17,58 +17,84 @@ def readData(file, ctype = True,seperator = ":",ignore=True) -> dict:
     :type ignore: bool
     """
 
-    
+    #name variable for storing name of the key (temp)
     name = []
+    #value variable for storing value (temp)
     value = []
-    d = dict()
-    isvalue  = False
-    com = False
+    #dict to return the user
+    data = dict()
+    #check if current word is a Value
+    isValue  = False
+    isComment = False
+    #if it is a json file then use json library to load it
     if ('.json' or '.Json')in file.name:
-        
         return json.load(file)
-
+    #if not then check every line in the file
     for line in file:
+        #if the line is empty then continue to next line
         if bool(line.strip()) == False:
             continue
+        #then check every word in the line
         for word in line.strip():
+            #if the word is comment then skip this line and
+            #set com to true
             if word == "#":
-                com = True
+                isComment = True
                 break
-            elif word != seperator and isvalue == False:
+            #if the word isn't a seperator and is not value
+            elif word != seperator and isValue == False:
+                #then add the word to the name variable
                 name.append(word.strip())
+            #if the word is a seperator then set isvalue to true
             elif word == seperator:
-                isvalue = True
-            elif isvalue:
+                isValue = True
+            #if the word is value then append the word
+            #to the value variable
+            elif isValue:
                 value.append(word.strip())
-        n ="".join(name)
-        v = "".join(value)
-        if com:
-            com = False
-        elif com==False and ctype == False:
-            n ="".join(name)
-            v = "".join(value)
-            d[n.strip()] = v.strip()
+        #after checking every word in that line add the key
+        #and the value to the dict variable
+        joined_name ="".join(name)
+        joined_value = "".join(value)
+        #if isComment is true then set it to false and 
+        #go to next line
+        if isComment:
+            isComment = False
+        #if it is not a comment and we don't have ctype enabled then
+        elif isComment==False and ctype == False:
+            #join the key and value and add them to dict variable and reset every variable
+            joined_name ="".join(name)
+            joined_value = "".join(value)
+            data[joined_name.strip()] = joined_value.strip()
             name = []
             value = []
-            isvalue = False
-            if ignore == False and v.strip() == '':
+            isValue = False
+            #if ignore is not enabled and the value is empty then throw and error
+            if ignore == False and joined_value.strip() == '':
                 raise Exception("Incorrect data in the file")
-        elif com == False and ctype:
-            n ="".join(name)
-            v = "".join(value)
+        #if is not a comment and ctype is enabled then
+        elif isComment == False and ctype:
+            #join the key and value
+            joined_name ="".join(name)
+            joined_value = "".join(value)
+            #try if value is an int
             try:
-                int(v)
-                d[n.strip()] = int(v.strip())
+                int(joined_value)
+                #if it is then add them to dict variable and reset every variable 
+                data[joined_name.strip()] = int(joined_value.strip())
                 name = []
                 value = []
-                isvalue = False
-                if ignore == False and v.strip() == '':
+                isValue = False
+                #if ignore is not enabled and the value is empty then throw and error
+                if ignore == False and str(joined_value.strip()) == '':
                    raise Exception("Incorrect data in the file")
+            #if it is not an int then add them to dict variable and reset every variable  
             except:
-                d[n.strip()] = v.strip()
+                data[joined_name.strip()] = joined_value.strip()
                 name = []
                 value = []
-                isvalue = False
-                if ignore == False and v.strip() == '':
+                isValue = False
+                if ignore == False and joined_value.strip() == '':
                    raise Exception("Incorrect data in the file")
-    return d    
+    #after every line is checked return the user the dict variable
+    return data    
